c4692508a9333b0181e4dad7e33bee493db5cd72
gcd418 <patosquare418@gmail.com>
2025-02-21 18:26:05 -0400

    feat: saludar de acuerdo a la hora

--- src/index.js
+++ src/index.js
@@ -2,8 +2,22 @@ const nombre = document.querySelector("#nombre");
 const saludarButton = document.querySelector("#saludar_button");
 const div = document.querySelector("#saludo");
 
+const saludos = ["Buenos días", "Buenas tardes", "Buenas noches"];
+
 saludarButton.addEventListener("click", (event) => {
   event.preventDefault();
+  let fechaActual = new Date();
+  let hora = fechaActual.getHours();
   const nombreSaludo = nombre.value;
-  div.innerHTML = "<p>" + "Hola " + nombreSaludo + "</p>";
+  div.innerHTML = "<p>" + saludarPorHora(7) + nombreSaludo + "</p>";
 });
+
+function saludarPorHora(hora) {
+  if (hora >= 6 && hora < 12) {
+    return "Buenos días ";
+  } else if (hora >= 12 && hora < 20) {
+    return "Buenas tardes ";
+  } else {
+    return "Buenas noches ";
+  }
+}
